827111cc00b7c23f88fbfab67b3816155a026a49
jaylen.chan <jaylen.chan@hotmail.com>
2022-05-15 12:46:48 +0800

    feat: 检查node版本

---
+++ core/cli/lib/const.js
@@ -0,0 +1,1 @@
+exports.LOWEST_NODE_VERSION = '12.0.0'
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,14 +1,31 @@
-const pkg = require('../package.json')
+const semver = require('semver')
+const colors = require('colors')
 const log = require('@tool-cli/log')
+const pkg = require('../package.json')
+const { LOWEST_NODE_VERSION } = require('./const')
 
 class Cli {
   runCli() {
-    this.checkPkgVersion()
+    try {
+      this.checkPkgVersion()
+      this.checkNodeVersion()
+    } catch (e) {
+      log.error(e.message)
+    }
   }
 
   checkPkgVersion() {
-    const version = pkg.version
-    log.notice('当前tool-cli版本:', version)
+    const pkgVersion = pkg.version
+    log.notice('当前tool-cli版本:', pkgVersion)
+  }
+
+  checkNodeVersion() {
+    // 获取当前版本号
+    const curNodeVersion = process.version
+    // 对比当前版本号和脚手架允许的最低版本号
+    if (!semver.gte(curNodeVersion, LOWEST_NODE_VERSION)) {
+      throw new Error(colors.red('当前node版本号低于tool-cli支持的最低node版本号'))
+    }
   }
 }
 
